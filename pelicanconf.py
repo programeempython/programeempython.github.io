3cafc7b73d1ddda6a07e62da26eb19cf294b5ead
Julio Melanda <jcemelanda@localhost.localdomain>
2017-10-13 22:05:03 -0300

    Tries to fix CNAME overriding

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,3 +32,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+STATIC_PATHS = ['extra']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}

0e547dc7c2efa5d257f34a6ecb631f217f77b953
Julio Melanda <jcemelanda@gmail.com>
2022-08-07 22:24:32 +0200

    Fixing ads

--- publishconf.py
+++ publishconf.py
@@ -23,6 +23,14 @@ GOOGLE_TAG = "G-CWQ34LZTCX"
 GOOGLE_ADSENSE = {
     'ca_id': 'ca-pub-5019623326305470',
     'page_level_ads': True,
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
 }
 
 DISQUS_SITENAME = "programeempython"
